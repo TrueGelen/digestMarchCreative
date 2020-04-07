2ca4f7a4f1be3f0afd3f902e183faa76d3bd6382
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-04-07 17:36:43 +0300

    pofition changer for logo and burger was added

--- source/js/logo.js
+++ source/js/logo.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
 	const logo = document.querySelector('.fixedLogo')
 	const wrapper = document.querySelector('#wrapper')
+	const slide = wrapper.querySelector('.slide')
 
 	function getPropertyOfElement(element, property) {
 		if (element.style[property] === '') {
@@ -10,14 +11,32 @@ window.addEventListener('load', () => {
 		}
 	}
 
+	// function buildLogo() {
+	// 	let clientHeight = window.innerHeight
+	// 	let wrapperHeight = parseInt(getPropertyOfElement(wrapper, 'height'))
+
+	// 	if (window.innerWidth > 768) {
+	// 		let top = window.innerWidth > 1050 ?
+	// 			((clientHeight - wrapperHeight) / 2) + (wrapperHeight / 100 * 8) :
+	// 			((clientHeight - wrapperHeight) / 2) + (wrapperHeight / 100 * 7)
+	// 		logo.style.top = `${top}px`
+	// 	} else {
+	// 		logo.style.top = ''
+	// 	}
+	// }
+
 	function buildLogo() {
 		let clientHeight = window.innerHeight
 		let wrapperHeight = parseInt(getPropertyOfElement(wrapper, 'height'))
-
 		if (window.innerWidth > 768) {
-			let top = window.innerWidth > 1050 ?
-				((clientHeight - wrapperHeight) / 2) + (wrapperHeight / 100 * 8) :
-				((clientHeight - wrapperHeight) / 2) + (wrapperHeight / 100 * 7)
+			let top = null
+			if (window.pageXOffset > (slide.getBoundingClientRect().width / 2)) {
+				top = window.innerWidth > 1450 ?
+					((clientHeight - wrapperHeight) / 2) + (wrapperHeight / 100 * 7) :
+					((clientHeight - wrapperHeight) / 2) + 10
+			} else {
+				top = ((clientHeight - wrapperHeight) / 2) + (wrapperHeight / 100 * 7)
+			}
 			logo.style.top = `${top}px`
 		} else {
 			logo.style.top = ''
@@ -25,6 +44,11 @@ window.addEventListener('load', () => {
 	}
 
 	buildLogo()
+
+	window.addEventListener('scroll', () => {
+		if (window.pageXOffset < slide.getBoundingClientRect().width)
+			buildLogo()
+	})
 
 	window.addEventListener('resize', () => {
 		buildLogo()
